58b697448aee85059cbca416104eda341d48d4a2
demonicirfan <hashtag.irfan@gmail.com>
2021-08-22 15:15:10 +0530

    loops

--- src/com/irfan/Main.java
+++ src/com/irfan/Main.java
@@ -1,8 +1,14 @@
 package com.irfan;
 
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+      int num =1;
+      while (num<=5){
+          System.out.println(num);
+          num+=1;
+      }
     }
 }
